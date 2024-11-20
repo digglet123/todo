8e5102585fc6d6f95c1c63e8e3c2c12b55ba6940
Mikko Pohjalainen <mikko.pohjalainen@funidata.fi>
2024-11-20 16:44:06 +0200

    Added angular app skeleton and added angular app to start.sh and docker-compose.yml

--- start.sh
+++ start.sh
@@ -2,8 +2,13 @@
 
 cd ./todo-api
 ./mvnw clean install
-docker build -t example/todo .
+docker build -t todo-api .
 cd ..
+
+cd ./todo-app
+docker build -t todo-app .
+cd ..
+
 docker compose up -d
 
 
